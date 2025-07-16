64702b5fdfa78db3c3b1abace2fe25ca70aa34ee
mrb <michaelbarrett@bluelily.com.au>
2025-07-16 20:56:32 +1000

    restore

--- services/__init__.py
+++ services/__init__.py
@@ -1,18 +1,8 @@
-
 """
 Services package for Brisbane Property Intelligence
 """
 
 from .llm_service import LLMService
 from .property_service import PropertyAnalysisService
-from .stability_service import StabilityService
-from .hugging_face_service import HuggingFaceService
-from .mailchannels_service import MailChannelsService
 
-__all__ = [
-    'LLMService', 
-    'PropertyAnalysisService',
-    'StabilityService',
-    'HuggingFaceService',
-    'MailChannelsService'
-]
+__all__ = ['LLMService', 'PropertyAnalysisService']
